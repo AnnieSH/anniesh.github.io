037d4abd58c230379c331712dc628f1d430430ce
Annie Hu <annie.hu.ah@gmail.com>
2019-12-28 14:54:57 -0800

    Updates

---
+++ precache-manifest.ecdb0dd7b609d27d0e5b594d14dc0923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93835d67a6a0e270780285d8ed95ae55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9db85a655147e34a7ad8",
+    "url": "/static/css/main.f4d0dfac.chunk.css"
+  },
+  {
+    "revision": "8d7dae674c65b5bcc07d",
+    "url": "/static/js/2.37919ec7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.37919ec7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9db85a655147e34a7ad8",
+    "url": "/static/js/main.e7f62cd4.chunk.js"
+  },
+  {
+    "revision": "3cf5aa946f136cbe9794",
+    "url": "/static/js/runtime-main.d55292af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
